ffd54220c833aedd55a1cebb17c5081639904a98
MadanPantrangam <85565216+MadanPantrangam@users.noreply.github.com>
2021-09-08 15:52:27 +0530

    Add files via upload

---
+++ Day03/Employee1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day01.Day03
+{
+    class Employee1
+    {
+        static void Main(string[] args)
+        {
+            Employee obj = new Employee();
+            Console.WriteLine("Please Enter Employee Details : ");
+            Console.WriteLine("=========================");
+            Console.WriteLine("Enter Employee Id : ");
+            obj.EmpID = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Employee Name : ");
+            obj.EmpName = Console.ReadLine();
+            Console.WriteLine("Enter Employee Address : ");
+            obj.EmpAddress = Console.ReadLine();
+            Console.WriteLine("Enter Employee DOB : ");
+            obj.EmpDOB = Console.ReadLine();
+            Console.WriteLine($"Emplyee Details : \n EmpId : {obj.EmpID} \nEmployee Name : {obj.EmpName}\n Employee Address : {obj.EmpAddress} \nEmployee DOB : {obj.EmpDOB}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day03/EmployeesObj.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Day01.Day03
+{
+    class EmployeesObj
+    {
+        static void Main(string[] args)
+        {
+            EmployeeData();
+            Console.ReadLine();
+        }
+
+        private static void EmployeeData()
+        {
+            var Employees = new Employee[5];
+            Employees[0] = new Employee { EmpID = 1, EmpName = "Madan", EmpAddress = "Nellore", EmpDOB = "10-05-1999" };
+            Employees[1] = new Employee { EmpID = 2, EmpName = "Suneel", EmpAddress = "Bangalore", EmpDOB = "15-07-1996" };
+            Employees[2] = new Employee { EmpID = 3, EmpName = "Ram", EmpAddress = "Sullurpeta", EmpDOB = "02-10-1995" };
+            Employees[3] = new Employee { EmpID = 4, EmpName = "Sam", EmpAddress = "Kadapa", EmpDOB = "01-01-2000" };
+            Employees[4] = new Employee { EmpID = 5, EmpName = "Rahul", EmpAddress = "Vizag", EmpDOB = "11-06-1999" };
+
+            foreach (var employee in Employees)
+            {
+                Console.WriteLine($"Employee Id : {employee.EmpID}\n Employee Name : {employee.EmpName}\nEmployee Address : {employee.EmpAddress}\nEmployee DOB : {employee.EmpDOB}");
+                Console.WriteLine("================================================");
+            }
+        }
+    }
+}
